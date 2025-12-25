bb044b7cf87856eab2355b8832227195b7345e32
meimeiartsy-gif <meimeiartsy@gmail.com>
2025-12-26 01:42:39 +0800

    Add fetch method to worker for response handling

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,8 @@
-
+export default {
+  async fetch(request, env, ctx) {
+    return new Response(
+      "Shop Mini App is running",
+      { headers: { "content-type": "text/plain" } }
+    );
+  }
+};
